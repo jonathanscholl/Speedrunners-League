81b1b93488dba323d1ffbff791396e7b656c9af6
jonischolll <152484485+jonischolll@users.noreply.github.com>
2024-04-08 14:40:19 +0200

    Menu option on controller
    open menu from controller with get button down menu (option button on controller and escape on keyboard)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -73,7 +73,7 @@ public class PlayerMovement : MonoBehaviour
     {
 
 
-        if (Input.GetKey(KeyCode.Escape))
+        if (Input.GetButtonDown("Menu"))
 
         {
             SceneManager.LoadSceneAsync(0);
